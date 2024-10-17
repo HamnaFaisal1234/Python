b575c7ed01c8fc97b2df8f52a4e1b93e11381a82
HamnaFaisal1234 <umehafsa66@gmail.com>
2024-10-17 20:33:19 +0500

    Functions (class activities)

---
+++ Functions in python/third.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    print(x+y)
+add(5,6)
+
+def sub(x,y):
+    print(x-y)
+sub(5,6)
+
+def multiply(x,y):
+    print(x*y)
+multiply(5,6)
+
+def division(x,y):
+    print(x/y)
+division(5,6)
