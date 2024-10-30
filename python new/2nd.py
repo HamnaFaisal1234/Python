4a24ef834e0808ffa1ce6b58aa9a9511420b51b5
HamnaFaisal1234 <umehafsa66@gmail.com>
2024-10-30 20:19:30 +0500

    Data Structures 2

---
+++ python new/2nd.py
@@ -0,0 +1,8 @@
+set1={"Hamna" , "Aisha" , "Ahsan" , "Shreemann" , "Aksh"}
+set2={"Aisha" , "Uma" , "Codingal"}
+print(set1)
+print("Orignal set:  ",set1)
+print("Union set:  ",set1.union(set2))
+print("Common set:  ",set1.intersection(set2))
+print("Difference:  ",set1.difference(set2))
+print("Symmetric Difference:  ",set1.symmetric_difference(set2))
---
+++ python new/first.py
@@ -0,0 +1,4 @@
+tuple = ("Hamna" , "Faisal" , "Asiya")
+print(tuple)
+print(tuple[0])
+print(tuple[1:4])
