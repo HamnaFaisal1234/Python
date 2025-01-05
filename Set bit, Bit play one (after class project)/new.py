bfadd6d2daaf4d7830c0832bb5feb923a295777e
HamnaFaisal1234 <umehafsa66@gmail.com>
2025-01-05 11:55:24 +0500

    after class project

---
+++ Set bit, Bit play one (after class project)/new.py
@@ -0,0 +1,8 @@
+bits=int(input('Enter a number:'))
+binary_representation = bin(bits)[2:] 
+print(binary_representation)
+position = 1
+while bits & 1 == 0:  
+        bits = bits >> 1
+        position += 1
+print('Position of the first set bit:',position)  
